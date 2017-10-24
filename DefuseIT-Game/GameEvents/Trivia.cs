870e8f179db0c427d06c7022c700339e9ceca8e8
Dominic K <ScienceARK@users.noreply.github.com>
2017-10-24 20:34:43 +0200

    Niet kijken
    serieus niet doen

---
+++ DefuseIT-Game/GameEvents/Trivia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefuseIT_Game.GameEvents
+{
+    class Trivia
+    {
+        //Wie dit leest vraag zich vast af waarom dit leeg is, nou dit is het class bestand waar ik de uiteindelijke versie van trivia in zal zetten om het wat netter te houden :) :C
+    }
+}
